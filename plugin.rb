a09a4e2b0c985c3c31219704ecd3331df97aa7b5
Felix Ungman <felix@ungman.com>
2018-01-01 21:06:25 +0100

    Add apikey check

--- plugin.rb
+++ plugin.rb
@@ -1,30 +1,14 @@
 # name: warstage
 # about: warstage integration plugin
-# version: 0.9.1
+# version: 1.0.0
 # authors: Felix Ungman
-# url: https://github.com/nikodil/userauth
+# url: ssh://git-codecommit.us-east-1.amazonaws.com/v1/repos/warstage-discourse
 
 enabled_site_setting :warstage_enabled
 
 PLUGIN_NAME ||= "warstage".freeze
 
 after_initialize do
-
-  #ApplicationController.class_eval do
-  #  before_filter :ensure_embeddable
-  #  def ensure_embeddable
-  #    origin = SiteSetting.openwar_origin
-  #    domains = SiteSetting.openwar_domains
-  #    response.headers['X-Frame-Options'] = "ALLOW-FROM #{origin}"
-  #    response.headers['Content-Security-Policy'] = "default-src 'self' #{domains};" +
-  #      " script-src 'self' #{domains} data: 'unsafe-inline' 'unsafe-eval';" +
-  #      " style-src 'self' #{domains} 'unsafe-inline';" +
-  #      " img-src 'self' #{domains} data:;" +
-  #      " font-src 'self' #{domains} data:;" +
-  #      " media-src 'self' #{domains} data:;" +
-  #      " frame-ancestors 'self' #{domains};"
-  #  end
-  #end
 
   module ::Warstage
     class Engine < ::Rails::Engine
@@ -38,24 +22,31 @@ after_initialize do
   class Warstage::UserController < ::ApplicationController
     requires_plugin PLUGIN_NAME
 
-    #before_filter :ensure_logged_in, except: [:token]
-
     def verify
-      user = fetch_user_from_params(include_inactive: current_user.try(:staff?))
-      user = nil unless user.confirm_password?(params[:password])
-      render json: user_to_json(user)
-    end
-
-    def lookup
-      user = if params[:email]
-         User.find_by_email(params[:email])
-      else
-        fetch_user_from_params(include_inactive: current_user.try(:staff?))
+      user = nil
+      if params[:apikey] == SiteSetting.warstage_apikey
+        begin
+          user = fetch_user_from_params(include_inactive: current_user.try(:staff?))
+          user = nil unless user.confirm_password?(params[:password])
+        rescue Discourse::NotFound
+        end
       end
       render json: user_to_json(user)
     end
 
-    def grant_badge
+    def lookup
+      user = nil
+      if params[:apikey] == SiteSetting.warstage_apikey
+        begin
+          user = if params[:email]
+             User.find_by_email(params[:email])
+          else
+            fetch_user_from_params(include_inactive: current_user.try(:staff?))
+          end
+        rescue Discourse::NotFound
+        end
+      end
+      render json: user_to_json(user)
     end
 
     def user_to_json(user)
@@ -72,7 +63,7 @@ after_initialize do
           avatar_url: user.avatar_template.gsub('{size}', '45')
         }
       else
-        {errors:["no user"]}
+        {}
       end
     end
   end
@@ -80,7 +71,6 @@ after_initialize do
   Warstage::Engine.routes.draw do
     get '/user/verify' => 'user#verify'
     get '/user/lookup' => 'user#lookup'
-    get '/user/grant_badge' => 'user#grant_badge'
   end
 
   Discourse::Application.routes.append do
